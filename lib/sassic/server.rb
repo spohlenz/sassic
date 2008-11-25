91730e7aa5fa5a1ddf686e9ac7ad646164801c61
Sam Pohlenz <sam@gooddogdesign.com>
2008-11-25 09:24:44 -0800

    Added assets to static dirs list

--- lib/sassic/server.rb
+++ lib/sassic/server.rb
@@ -19,7 +19,7 @@ private
       use Rack::ShowExceptions
       use Rack::ShowStatus
       
-      use Rack::Static, :urls => [ "/images", "/javascripts" ]
+      use Rack::Static, :urls => [ "/images", "/javascripts", "/assets" ]
       
       map '/' do
         run Sassic::Handlers::TemplateHandler.new
@@ -34,15 +34,4 @@ private
   def handler
     Rack::Handler::Mongrel
   end
-  
-  # def setup_mount_points(s)
-  #   setup_static_mount_point(s, '/javascripts')
-  #   setup_static_mount_point(s, '/images')
-  #   s.mount("/stylesheets", Sassic::Servlets::SassServlet)
-  #   s.mount("/", Sassic::Servlets::TemplateServlet)
-  # end
-  # 
-  # def setup_static_mount_point(s, folder)
-  #   s.mount(folder, HTTPServlet::FileHandler, Dir::pwd + folder)
-  # end
 end
